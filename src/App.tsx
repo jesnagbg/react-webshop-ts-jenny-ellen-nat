797f562cf547ab04e3998a85cedf9ad43ab83b54
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-03-13 19:58:22 +0100

    Add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hello Students, begin here!</h1>
       <Outlet />
     </div>
