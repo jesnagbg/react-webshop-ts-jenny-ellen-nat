f068d3c51841d2a66c50de9379da5b50d7e64274
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-03-13 19:58:09 +0100

    base for cart and header componentsts

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,5 +1,25 @@
-import {Box} from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import {Badge, BadgeProps, IconButton, styled} from "@mui/material";
 
 export default function CartIcon() {
-  return <Box></Box>;
+  return (
+    <IconButton aria-label="cart" sx={{padding: "1rem"}}>
+      <StyledBadge badgeContent={4} sx={{color: "#333"}}>
+        <ShoppingCartOutlinedIcon sx={{color: "#333"}} />
+      </StyledBadge>
+    </IconButton>
+  );
 }
+
+const StyledBadge = styled(Badge)<BadgeProps>(({theme}) => ({
+  "& .MuiBadge-badge": {
+    vertical: "top",
+    horizontal: "right",
+    background: `#c8e1c0`,
+    border: `2px solid #333`,
+    fontSize: `10px`,
+    padding: "2px",
+    height: 14,
+    minWidth: 14,
+  },
+}));
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,69 @@
-import {Box} from "@mui/material";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Slide,
+  Typography,
+} from "@mui/material";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import {FC, ReactElement} from "react";
+import CartIcon from "./CartIcon";
+
+// Define the type for the HideOnScrollProps
+type HideOnScrollProps = {
+  children: ReactElement;
+};
+
+// Define the HideOnScroll component as a function component
+const HideOnScroll: FC<HideOnScrollProps> = ({children}) => {
+  // useScrollTrigger returns a boolean value indicating whether the user has scrolled down the page
+  const trigger = useScrollTrigger();
+
+  // We use the Slide component to add a slide animation to the AppBar
+  // The 'appear' prop determines whether the component should animate on first render
+  // The 'direction' prop determines the direction of the slide animation
+  // The 'in' prop determines whether the component should be visible or not
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+};
 
 export default function Header() {
-  return <Box></Box>;
+  return (
+    <HideOnScroll>
+      <AppBar
+        sx={{
+          height: 80,
+          bgcolor: "#fff",
+          marginBottom: "100px",
+          boxShadow: "none",
+        }}
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            color: "#333",
+            height: "100%",
+          }}
+        >
+          {/* <NavLink to="/">Start page</NavLink> */}
+          <Typography variant="h6">Piece by Piece</Typography>
+          <Box>
+            <CartIcon />
+            <IconButton>
+              <PersonOutlineOutlinedIcon
+                sx={{color: "#333", fontSize: "1.7rem"}}
+              />
+            </IconButton>
+          </Box>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
+  );
 }
