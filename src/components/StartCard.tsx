71cf44d67a98e0d1cb616ff16ef743714045813d
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-03-14 18:37:11 +0100

    sorta works

--- src/components/StartCard.tsx
+++ src/components/StartCard.tsx
@@ -1,5 +1,31 @@
-import {Box} from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
-export default function StartCard() {
-  return <Box></Box>;
+interface Props {
+  id?: string;
+  image?: string;
+  title: string;
+  price?: number;
+}
+
+export default function StartCard({ title, image }: Props) {
+  return (
+    <Box>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia sx={{ height: 140 }} image={image} title="green iguana" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
 }
--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,134 @@
+// export interface Product {
+//   id: string;
+//   image: string;
+//   title: string;
+//   description: string;
+//   price: number;
+// }
+
+// export const products: Product[] = [];
+
 export interface Product {
   id: string;
   image: string;
+  images?: string[];
   title: string;
   description: string;
   price: number;
 }
 
-export const products: Product[] = [];
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export const products: Product[] = [
+  {
+    id: "000001",
+    image: `./assets/calmwoman.JPG`,
+    images: [
+      "./assets/calmwoman2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Calm woman",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000002",
+    image: "./assets/daisies.JPG",
+    images: [
+      "./assets/daisies.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Daisies",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 349,
+  },
+  {
+    id: "000003",
+    image: "./assets/moby.JPG",
+    images: [
+      "./assets/moby2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Moby",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000004",
+    image: "./assets/sunriseferns.JPG",
+    images: [
+      "./assets/sunriseferns2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Sunrise ferns",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000005",
+    image: "./assets/makingfriends.JPG",
+    images: [
+      "./assets/makingfriends2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Making friends",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000006",
+    image: "./assets/checkerboard.JPG",
+    images: [
+      "./assets/checkerboard2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Checkerboard",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 499,
+  },
+  {
+    id: "000007",
+    image: "./assets/discoathome.JPG",
+    images: [
+      "./assets/discoathome2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Disco at home",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000008",
+    image: "./assets/villagefarmersmarket.JPG",
+    images: [
+      "./assets/villagefarmersmarket2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "Village farmers market",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 399,
+  },
+  {
+    id: "000009",
+    image: "./assets/californialiving.JPG",
+    images: [
+      "./assets/californialiving2.JPG",
+      "./assets/third.jpg",
+    ],
+    title: "California living",
+    description:
+      "Our jigsaw puzzles transform your favorite artists' designs into hours of tabletop fun. Exercise your mind with our precision cut puzzles featuring a thick, class-leading paperboard, with a satin finish for added durability. Available in either 200, 500 or 1,000 piece sets.",
+    price: 349,
+  },
+];
+
