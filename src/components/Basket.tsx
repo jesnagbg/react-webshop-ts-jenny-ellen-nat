ab34806b77b268cdeccd85fa119dab586093ba50
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-03-15 15:31:08 +0100

    Card rendering with correct structure.

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,9 +1,22 @@
-import {Box, Typography} from "@mui/material";
+import {Box, Divider, Stack, Typography} from "@mui/material";
+import {useCart} from "../contexts/CartContext";
+import CheckoutCard from "./CheckoutCard";
 
 export default function Basket() {
+  const {cartItems} = useCart();
+
   return (
     <Box>
       <Typography variant="h5">Basket</Typography>
+      <Divider />
+      <Stack spacing={2}>
+        {cartItems.map((item) => (
+          <Box>
+            <CheckoutCard item={item} />
+            <Divider />
+          </Box>
+        ))}
+      </Stack>
     </Box>
   );
 }
--- src/components/CheckoutCard.tsx
+++ src/components/CheckoutCard.tsx
@@ -1,5 +1,57 @@
-import {Box} from "@mui/material";
+import {Clear} from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Fab,
+  Typography,
+} from "@mui/material";
+import {CartItem} from "../data";
 
-export default function CheckoutCard() {
-  return <Box></Box>;
+interface Props {
+  item: CartItem;
 }
+
+export default function CheckoutCard({item}: Props) {
+  return (
+    <Card sx={styledCard}>
+      <CardMedia
+        sx={styledCardMedia}
+        component="img"
+        image={item.image}
+        alt="Puzzle thumbnail."
+      />
+      <Box sx={styledBox}>
+        <CardContent>
+          <Typography variant="h5">{item.title}</Typography>
+          <Typography variant="body1">Number pieces?</Typography>
+          <Typography variant="body1">Change quantity buttons here.</Typography>
+          <Typography variant="body1">{item.price}kr</Typography>
+        </CardContent>
+      </Box>
+      <Fab
+        color="primary"
+        aria-label="remove"
+        size="small"
+      >
+        <Clear />
+      </Fab>
+    </Card>
+  );
+}
+
+const styledCard = {
+  display: "flex",
+  dropShadow: "none",
+};
+
+const styledCardMedia = {
+  maxWidth: 0.3,
+};
+
+const styledBox = {
+  display: "flex",
+  flexDirection: "column",
+  flex: "1",
+};
