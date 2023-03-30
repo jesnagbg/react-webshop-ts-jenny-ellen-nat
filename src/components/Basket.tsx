1ba78ccd73e5186db18520e3c9ebe6272011f380
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-03-30 15:58:16 +0200

    Empty cart message implemented and styled.

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,25 +1,59 @@
-import { Box, Divider, Stack, Typography } from '@mui/material';
+import { Box, Button, Divider, Stack, Typography } from '@mui/material';
+import { Theme } from '@mui/system';
+import { useNavigate } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
+import { theme } from '../theme';
 import CheckoutCard from './CheckoutCard';
 
 export default function Basket() {
   const { cartItems } = useCart();
 
+  const navigate = useNavigate();
+
+  const countryRoad = () => {
+    navigate('/');
+  };
+
   return (
     <Box>
-      <Typography variant="h5">Basket</Typography>
+      <Typography variant="h5">Cart</Typography>
       <Divider />
-      <Stack>
-        {cartItems.map((item) => (
-          <Box key={item.id}>
-            <CheckoutCard
-              item={item}
-              readonly={false}
-            />
-            <Divider />
+      {cartItems.length ? (
+        <Stack>
+          {cartItems.map((item) => (
+            <Box key={item.id}>
+              <CheckoutCard
+                item={item}
+                readonly={false}
+              />
+              <Divider />
+            </Box>
+          ))}
+        </Stack>
+      ) : (
+        <Box sx={styledEmptyFrame}>
+          <Box sx={styledEmptyBox(theme)}>
+            <Typography
+              variant="h5"
+              sx={styledEmptyText}
+            >
+              Your cart is empty!
+            </Typography>
+            <Typography
+              variant="h5"
+              sx={styledEmptyText}
+            >
+              :(
+            </Typography>
+            <Button
+              onClick={countryRoad}
+              sx={styledButton}
+            >
+              FILL IT
+            </Button>
           </Box>
-        ))}
-      </Stack>
+        </Box>
+      )}
       <Typography
         variant="subtitle1"
         sx={styledTypography}
@@ -35,6 +69,37 @@ export default function Basket() {
     </Box>
   );
 }
+
+const styledEmptyFrame = {
+  position: 'relative',
+  paddingTop: '100%',
+};
+
+const styledEmptyBox = (theme: Theme) => {
+  return {
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    left: '0',
+    right: '0',
+    backgroundColor: theme.palette?.secondary.main,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '1rem,',
+  };
+};
+
+const styledEmptyText = {
+  marginTop: '0.5rem',
+  textAlign: 'center',
+};
+const styledButton = {
+  marginTop: '1rem',
+  border: '1px solid black',
+  color: 'black',
+};
 
 const styledTypography = {
   textAlign: 'right',
